dfd4b136b7f2d603b7f05f56eeba9d5d271fe0ee
Xuyang, Zhang <zhang.7762@osu.edu>
2019-02-21 02:27:29 -0500

    unity user login/signup

---
+++ PusheenRun/Assets/Scripts/UserLogin.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UserLogin : MonoBehaviour
+{
+    private FirebaseAuth auth;
+    public InputField Email, Password;
+    public Button SignUp, LoginButton;
+    public Text ErrorText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+        //Just an example to save typing in the login form
+        SignUp.onClick.AddListener(() => Signup(Email.text, Password.text));
+        LoginButton.onClick.AddListener(() => Login(Email.text, Password.text));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Signup(string email, string password)
+    {
+        if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            //Error handling
+            return;
+        }
+
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync error: " + task.Exception);
+                if (task.Exception.InnerExceptions.Count > 0)
+                    UpdateErrorMessage(task.Exception.InnerExceptions[0].Message);
+                return;
+            }
+
+            FirebaseUser newUser = task.Result; // Firebase user has been created.
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+            UpdateErrorMessage("Signup Success");
+        });
+    }
+
+    private void UpdateErrorMessage(string message)
+    {
+        ErrorText.text = message;
+        Invoke("ClearErrorMessage", 3);
+    }
+
+    void ClearErrorMessage()
+    {
+        ErrorText.text = "";
+    }
+    public void Login(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync error: " + task.Exception);
+                if (task.Exception.InnerExceptions.Count > 0)
+                    UpdateErrorMessage(task.Exception.InnerExceptions[0].Message);
+                return;
+            }
+
+            FirebaseUser user = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                user.DisplayName, user.UserId);
+
+            PlayerPrefs.SetString("LoginUser", user != null ? user.Email : "Unknown");
+            SceneManager.LoadScene("StartMenu");
+        });
+    }
+}
